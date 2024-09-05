8b773410ecd07b0adaebb5acb034cf0eb38e5520
Diego <diego.vianna@rua.com.br>
2024-09-05 14:21:11 -0300

    feat(DDD) entidades e casos de uso: value object de slug

---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,24 @@
+export class Slug {
+  public value: string
+
+  constructor(value: string) {
+    this.value = value
+  }
+
+  /**
+   * Receives a string and normalize it as a slug
+   * 
+   * Example:"An example title" => "an-example-title"
+   * 
+   * @param text (string)
+   * @returns 
+   */
+  static createFromText(text: string) {
+    const slug = text.normalize("NFKD")
+      .toLowerCase()
+      .trim()
+      .replace(/[^a-z0-9]+/g, "-")
+
+    return new Slug(slug)
+  }
+}
