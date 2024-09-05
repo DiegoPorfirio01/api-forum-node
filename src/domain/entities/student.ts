44e37d8aa8cd5baa6eb0565cc5517ceb3b9c2f75
Diego <diego.vianna@rua.com.br>
2024-09-05 12:12:52 -0300

    init(DDD) entidades e casos de uso: primeiro caso de uso

---
+++ src/domain/entities/student.ts
@@ -0,0 +1,11 @@
+import { randomUUID } from "node:crypto"
+
+class Studant {
+  public id: string = randomUUID()
+  public name: string
+
+  constructor(name: string, id?: string) {
+    this.name = name
+    this.id = id ?? randomUUID()
+  }
+}
