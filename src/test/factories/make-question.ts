c4ac835421f231dea9c6753d8867c27a40fe5bcd
Diego <diego.vianna@rua.com.br>
2024-09-06 11:03:10 -0300

    feat(DDD with Clean Architeture) implements faker data

--- src/test/factories/make-question.ts
+++ src/test/factories/make-question.ts
@@ -1,15 +1,20 @@
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { Question, type QuestionProps } from "@/domain/forum/enterprise/entities/question";
-import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
+import { faker } from "@faker-js/faker";
 
-
-export function makeQuestion(override: Partial<QuestionProps> = {}): Question {
-  const question = Question.create({
-    authorId: new UniqueEntityId(),
-    title: 'any_title',
-    content: 'any_content',
-    ...override
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId
+): Question {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityId(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override
+    },
+    id,
+)
 
   return question
 }
