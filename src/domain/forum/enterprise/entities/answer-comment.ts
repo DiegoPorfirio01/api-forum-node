b20c64b03c24b8a80a6c4e5b20463d74953bdebf
Diego <diego.vianna@rua.com.br>
2024-09-08 12:52:17 -0300

    feat(DDD with clean architecture)  cria as entidades : Answer-comments / Question-comments

---
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -0,0 +1,62 @@
+import { Entity } from '@/core/entities/entity'
+import type { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import type { Optional } from '@/core/types/optional'
+import dayjs from 'dayjs'
+
+export interface AnswerCommentProps {
+  authorId: UniqueEntityId
+  answerId: UniqueEntityId
+  content: string
+  createdAt: Date
+  updatedAt?: Date
+}
+
+export class AnswerComment extends Entity<AnswerCommentProps> {
+  get content() {
+    return this.props.content
+  }
+
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get isNew() {
+    return dayjs().diff(this.props.createdAt, 'day') < 4
+  }
+
+  get summary() {
+    return this.props.content.substring(0, 120).trimEnd().concat('...')
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(value: string) {
+    this.props.content = value
+    this.touch()
+  }
+
+  static create(
+    props: Optional<AnswerCommentProps, 'createdAt'>,
+    id?: UniqueEntityId,
+  ) {
+    const answerComment = new AnswerComment(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return answerComment
+  }
+}
